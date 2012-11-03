7c41003ee0973279b76037259c69e1e1d071045e
Maurits Lamers <sproutcore@devmt.hku.nl>
2012-11-03 10:37:01 +0100

    allowing new users to register using captcha

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -11,3 +11,7 @@
 
 maurits:203705fef86d69db403c18d633622cb3:Maurits Lamers:maurits.lamers@kmt.hku.nl:admin,user
 enquora:$1$91500ecd$kwvv78FLSWMJ/8sYSQpVS0:David:david.richardson@enquora.com:user
+geoffreyd:$1$6e37f4be$iJX6JvJJ5Xi.MBC7wK9rf.:Geoffrey Donaldson:geoffreyd@gmail.com:user
+dcporter:$1$d8e55a48$9AC7x9GpmYrFXAerkNPk11:Dave Porter:dcporter@sproutcore.com:user
+ricolicous:$1$38c80062$H65fl0yf9PUcVPYeYQg5F.:Rico Jones:rico@jane.ai:user
+ricolicious:$1$d1ba1c62$uc4oMAUpXAZt3QqRIQU8Q1:Rico Jones:rico@jane.ai:user
