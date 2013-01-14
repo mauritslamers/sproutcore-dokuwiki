21a6b1b740a8793c90941d72198582ee0c4a1e07
root <root@devmt.hku.nl>
2013-01-15 00:16:11 +0100

    a few others

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -15,3 +15,4 @@ geoffreyd:$1$6e37f4be$iJX6JvJJ5Xi.MBC7wK9rf.:Geoffrey Donaldson:geoffreyd@gmail.
 dcporter:$1$d8e55a48$9AC7x9GpmYrFXAerkNPk11:Dave Porter:dcporter@sproutcore.com:user
 ricolicous:$1$38c80062$H65fl0yf9PUcVPYeYQg5F.:Rico Jones:rico@jane.ai:user
 ricolicious:$1$d1ba1c62$uc4oMAUpXAZt3QqRIQU8Q1:Rico Jones:rico@jane.ai:user
+joegaudet:$1$53f33921$p2vzJ0n3Rwsz71STP7FsD/:Joe Gaudet:joe@learndot.com:user
